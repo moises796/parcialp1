c6670534f182dfdf74abaeda5b66ae77a2d1321f
moises796 <moisesrivera796@gmail.com>
2018-02-24 14:24:34 -0600

    Ejercicio 1 Muy Pronto lanzamiento bot

--- examen.js
+++ examen.js
@@ -1,7 +1,9 @@
 var TelegramBot = require('node-telegram-bot-api'),
 telegram = new TelegramBot("520684278:AAEQK9CdBPuuBxWQJynoQSFVN822GSzDp6Y", { polling: true });
 
-
+telegram.on("text", (message) => {
+    telegram.sendMessage(message.chat.id, "Muy pronto lanzamiento bot");
+    });
 
     
 
