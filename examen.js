fe187f99d0adb400e4b9a261da36d27140fef7c1
IsKBnitez <kevineliasbenitez@gmail.com>
2018-02-24 14:30:24 -0600

    Ejercicio 5 Contestar cliente

--- examen.js
+++ examen.js
@@ -5,5 +5,7 @@ telegram.on("text", (message) => {
     telegram.sendMessage(message.chat.id, "Muy pronto lanzamiento bot");
     });
 
-    
+    telegram.onText(/\/hola/, (message) => {
+        telegram.sendMessage(message.chat.id, "Hola cliente "+message.from.first_name);
+    });
 
