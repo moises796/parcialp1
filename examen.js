abda285a81acf5d913e960be081cf7dde73c0964
slayer0963 <landos112@gmail.com>
2018-02-24 14:40:23 -0600

    Ejercicio 7 Comparar dos numeros

--- examen.js
+++ examen.js
@@ -8,4 +8,20 @@ telegram.on("text", (message) => {
     telegram.onText(/\/hola/, (message) => {
         telegram.sendMessage(message.chat.id, "Hola cliente "+message.from.first_name);
     });
+    telegram.onText( /\/mayor (.+) (.+)/, (msg, match) => {
+       
+       
+        const chatId = msg.chat.id;
+        const numero1 =  match[1]; 
+        const numero2 =  match[2]; 
+        if(numero1>numero2){
+            telegram.sendMessage(chatId,"El numero mayor es ======>"+ numero1);
+        }
+        else{
+            telegram.sendMessage(chatId,"El numero mayor es ======>"+ numero2);
+        }
+       
+        
+        
+      });
 
